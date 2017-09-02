f6a76d6720730c282ac99f060137e8f685bebf20
Silas Brasil <silasbrasil@workeduc.com>
2017-09-02 10:20:57 -0300

    Add has, take, currying examples

--- samples.js
+++ samples.js
@@ -34,6 +34,16 @@ const pow = x => x * 2;
 R.map(pow, [1, 2, 3]); // [2, 4, 6]
 R.map(pow, {x: 1, y: 2, z: 3}); // {x: 2, y: 4, z: 6}
 
+// Pick
+const person = {
+    name: 'Silas Brasil',
+    age: 15,
+    height: '3.5',
+    password: '1234'
+}
+R.pick(['name', 'age', 'password', 'token'], person)
+// { name: 'Silas Brasil', age: 15, password: '1234' }
+
 // PickAll
 const person = {
     name: 'Silas Brasil',
@@ -41,7 +51,41 @@ const person = {
     height: '3.5',
     password: '1234'
 }
-R.pickAll(['name', 'age', 'password'], person)
-// { name: 'Silas Brasil', age: 15, password: '1234' }
+R.pickAll(['name', 'age', 'password', 'token'], person)
+// { name: 'Silas Brasil', age: 15, password: '1234', token: undefined }
+
+// Has
+const hasName = R.has('name');
+hasName({name: 'alice'});   //=> true
+hasName({name: 'bob'});     //=> true
+hasName({});                //=> false
+
+// or
+R.has('name')({ name: 'John', age: 35 }) // => true
+R.has('name')({ age: 35 }) // => false
+
+// or
+const point = {x: 0, y: 0};
+const pointHas = R.has(R.__, point);
+pointHas('x');  //=> true
+pointHas('y');  //=> true
+pointHas('z');  //=> false
+
+// Take
+R.take(1, ['foo', 'bar', 'baz']); //=> ['foo']
+R.take(2, ['foo', 'bar', 'baz']); //=> ['foo', 'bar']
+R.take(3, ['foo', 'bar', 'baz']); //=> ['foo', 'bar', 'baz']
+R.take(4, ['foo', 'bar', 'baz']); //=> ['foo', 'bar', 'baz']
+R.take(3, 'ramda');               //=> 'ram'
 
 // Currying
+const printThree = (a, b, c) => console.log(a, b, c)
+const curryPrintThree = R.curry(printThree)
+
+const printRest = curryPrintThree('a', 'b')
+printC('c') //=> a, b, c
+
+// or
+
+curryPrintThree('a', 'b')('c') //=> a, b, c
+curryPrintThree('a')('b', 'c') //=> a, b, c
